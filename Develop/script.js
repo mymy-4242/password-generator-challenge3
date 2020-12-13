cb6c4a66ea61b1e781c0b2765e2b4a77a9408b3d
Local MM <73925149+mymy-4242@users.noreply.github.com>
2020-12-13 10:14:19 -0800

    number of characters prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,34 @@
 // Assignment code here
 
+//number of characters
+var generatePassword = function() {
+  //choose the number of characters
+  var characters = window.prompt(
+    "How many characters would you like this password to contain?"
+  );
+
+    if (characters < 8 || characters > 128) {
+      window.alert("Please choose a number from 8 - 128.");
+      return generatePassword();
+    } else {
+      var characters = array.length;
+      //save number of characters in session storage
+      sessionStorage.setItem("numberOfCharacters", characters);
+    }
+    console.log(characters);
+  
+  //choose to include lowercase characters
+  
+  //choose to include uppercase characters
+    
+  //choose in include numbers
+
+  //choose to include special characters
+  var specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '=', '-', '.', '!', '?', '<', '>', '@', '-', '_', ';', ':'];
+
+  //show results
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
