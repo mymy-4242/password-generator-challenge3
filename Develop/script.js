f7c69e4defc084fa88d5b22cf58eb78ce4cc74dc
Local MM <73925149+mymy-4242@users.noreply.github.com>
2020-12-13 11:55:37 -0800

    added prompts for uppercase and lowercase characters and attempted an output

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
-// Assignment code here
 
-//number of characters
-var generatePassword = function() {
   //choose the number of characters
   var characters = window.prompt(
     "How many characters would you like this password to contain?"
@@ -9,26 +6,59 @@ var generatePassword = function() {
 
     if (characters < 8 || characters > 128) {
       window.alert("Please choose a number from 8 - 128.");
-      return generatePassword();
-    } else {
-      var characters = array.length;
-      //save number of characters in session storage
-      sessionStorage.setItem("numberOfCharacters", characters);
     }
     console.log(characters);
   
   //choose to include lowercase characters
+    var lowerConfirm = confirm(
+      "Click OK to use lowercase letters in your password."
+    );
   
   //choose to include uppercase characters
-    
-  //choose in include numbers
+    var upperConfirm = confirm(
+      "Click OK to use uppercase letters in your password."
+    ); 
 
-  //choose to include special characters
-  var specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '=', '-', '.', '!', '?', '<', '>', '@', '-', '_', ';', ':'];
-
-  //show results
-  
+var generatePassword = function() {
+  var passCriteria = "";
+  if (lowerConfirm) {
+    passCriteria = "abcdefghijklmnopqrstuvwxyz";
+  } else if (upperConfirm) {
+    passCriteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+ 
+  var pass = ""
+  for (let i = 0; i < characters; i++) {
+    pass += passCriteria.charAt(Math.floor(Math.random() * passCriteria.characters));
+  }
+  return(pass);
 }
+  /*  
+  //choose in include numbers
+    var numConfirm = confirm(
+      "Click OK to use numbers in your password."
+    );
+    var num = []
+  //choose to include special characters
+    var specialCharConfirm = confirm(
+      "Click OK to use special characters in your password."
+    );
+
+    if (specialCharConfirm) {
+      var specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '=', '-', '.', '!', '?', '<', '>', '@', '-', '_', ';', ':'];
+      for (var i = 0; i < characters; i++) {
+        console.log(specialChar[i]);
+      }
+    }
+  //show results
+  var results = function(){
+    for (var i = 0; i < characters; ++i) {
+      console.log;
+    }
+  }
+    
+}
+*/
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -44,3 +74,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
